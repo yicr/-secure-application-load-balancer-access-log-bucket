93f057673abe7c136c4c214471fb4294e66a26a4
yicr <yicr@users.noreply.github.com>
2023-01-19 10:36:59 +0900

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,21 @@
+const { awscdk, javascript } = require('projen');
+
+const PROJECT_NAME = '@yicr/secure-application-load-balancer-access-log-bucket';
+const PROJECT_DESCRIPTION = 'AWS Application Load Balancer access log optimized secure bucket.';
+
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'yicr',
+  authorAddress: 'yicr@users.noreply.github.com',
+  cdkVersion: '2.43.0',
+  defaultReleaseBranch: 'main',
+  name: PROJECT_NAME,
+  description: PROJECT_DESCRIPTION,
+  repositoryUrl: 'https://github.com/yicr/secure-application-load-balancer-access-log-bucket.git',
+  keywords: ['aws', 'cdk', 'aws-cdk', 'elb', 'alb', 's3', 'bucket', 'access-log', 'log'],
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
---
+++ README.md
@@ -0,0 +1,4 @@
+# Secure Application Load Balancer access log bucket
+
+This is an AWS Application Load Balancer access log optimized secure bucket.
+
