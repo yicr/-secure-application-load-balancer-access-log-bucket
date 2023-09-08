45a8ae1737c5a29cac07af8f5aefd0ac651c0318
Yuichiro Higa <higa@avelio.co.jp>
2023-09-08 10:36:38 +0900

    fix!: rename package

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,24 +1,21 @@
 import { awscdk, javascript } from 'projen';
 
-const PROJECT_NAME = '@yicr/secure-application-load-balancer-access-log-bucket';
-const PROJECT_DESCRIPTION = 'AWS Application Load Balancer access log optimized secure bucket.';
-
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
   cdkVersion: '2.62.2',
   defaultReleaseBranch: 'main',
   projenrcTs: true,
-  name: PROJECT_NAME,
-  description: PROJECT_DESCRIPTION,
-  repositoryUrl: 'https://github.com/yicr/secure-application-load-balancer-access-log-bucket.git',
+  name: '@gammarer/aws-secure-application-load-balancer-access-log-bucket',
+  description: 'AWS Application Load Balancer access log optimized secure bucket.',
+  repositoryUrl: 'https://github.com/yicr/aws-secure-application-load-balancer-access-log-bucket.git',
   keywords: ['aws', 'cdk', 'aws-cdk', 'elb', 'alb', 's3', 'bucket', 'access-log', 'log'],
   npmAccess: javascript.NpmAccess.PUBLIC,
   deps: [
-    '@yicr/secure-log-bucket',
+    '@gammarer/aws-secure-log-bucket@~0.12.10',
   ],
   peerDeps: [
-    '@yicr/secure-log-bucket',
+    '@gammarer/aws-secure-log-bucket@~0.12.10',
   ],
   minNodeVersion: '16.0.0',
   workflowNodeVersion: '16.19.1',
